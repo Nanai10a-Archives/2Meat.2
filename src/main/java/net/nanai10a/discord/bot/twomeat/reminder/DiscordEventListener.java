909a011939c2d191373097ebb0314063050b0355
Nanai10a <Nanai10a@tutanota.com>
2020-06-26 01:17:41 +0900

    work
    + Rollbacked.

--- src/main/java/net/nanai10a/discord/bot/twomeat/reminder/DiscordEventListener.java
+++ src/main/java/net/nanai10a/discord/bot/twomeat/reminder/DiscordEventListener.java
@@ -1,9 +1,6 @@
 package net.nanai10a.discord.bot.twomeat.reminder;
 
-import net.dv8tion.jda.api.events.GenericEvent;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import org.checkerframework.checker.nullness.qual.NonNull;
 
 public class DiscordEventListener extends ListenerAdapter {
     private static DiscordEventListener[] listeners = new DiscordEventListener[0];
@@ -22,17 +19,4 @@ public class DiscordEventListener extends ListenerAdapter {
     /*
     以下にEventListenerとしての役割が実装される。
      */
-
-    @Override
-    public void onMessageReceived(MessageReceivedEvent event) {
-        for (DiscordEventListener listener : listeners) {
-            listener.EventAction(event, DiscordEvent.isMessageReceivedEvent);
-        }
-    }
-
-    public void EventAction(GenericEvent event, @NonNull DiscordEvent discordEvent, Object... others) {}
-}
-
-enum DiscordEvent {
-    isMessageReceivedEvent
 }
--- src/main/java/net/nanai10a/discord/bot/twomeat/reminder/ReminderTimer.java
+++ src/main/java/net/nanai10a/discord/bot/twomeat/reminder/ReminderTimer.java
@@ -1,24 +1,18 @@
 package net.nanai10a.discord.bot.twomeat.reminder;
 
-import net.dv8tion.jda.api.events.GenericEvent;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
-
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 
-public class ReminderTimer extends ListenerAdapter {
+public class ReminderTimer extends Object {
 
 
     private int id;
     private Timer timer = new Timer(true);
-    //private HashMap<String, >
 
     public ReminderTimer(int id) {
         this.id = id;
-        //DiscordEventListener.addListener(this);
     }
     public void addSchedule(TimerTask task, Date date) {
         timer.schedule(task, date);
@@ -27,24 +21,6 @@ public class ReminderTimer extends ListenerAdapter {
         //timer.schedule();
     }
     public void deleteSchedule() {
-
-    }
-
-    //@Override
-    public void EventAction(GenericEvent event, DiscordEvent discordEvent, Object... others) {
-
-    }
-}
-
-class ReminderTimerTask extends TimerTask {
-
-
-    public ReminderTimerTask(Notification notification, Object information) {
-
-    }
-
-    @Override
-    public void run() {
 
     }
 }
